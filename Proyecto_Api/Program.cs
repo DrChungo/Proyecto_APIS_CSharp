585e82a176679b823d0ab13d5033dca2b7a080fb
DrChungo <litingo1000@gmail.com>
2025-11-11 19:42:40 +0100

    Add main
    Hello world, i'm not yago

--- Proyecto_Api/Program.cs
+++ Proyecto_Api/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine("Emilio");
+﻿using System;
+
+namespace EjerciciosCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            System.Console.WriteLine("Hello world, I'm no Yago");
+        }
+    }
+}
