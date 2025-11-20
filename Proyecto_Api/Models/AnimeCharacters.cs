900b67c512d2bbda2c6ffe8d3eb29fa7f58f2429
Ruth <rcolladogarcia@iessonferrer.net>
2025-11-20 20:43:59 +0100

    Update models

--- Proyecto_Api/Models/AnimeCharacters.cs
+++ Proyecto_Api/Models/AnimeCharacters.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 
 namespace Models
 {
-    public class CharacterResponse
+    public class CharacterResponse 
     {
         [JsonPropertyName("data")]
-        public List<CharacterData> Data { get; set; }
+        public List<CharacterData>? Data { get; set; }
     }
 
     public class CharacterData
     {
         [JsonPropertyName("character")]
-        public Character Character { get; set; }
+        public Character? Character { get; set; }
 
         [JsonPropertyName("role")]
-        public string Role { get; set; }
+        public string? Role { get; set; }
 
         [JsonPropertyName("voice_actors")]
-        public List<VoiceActor> VoiceActors { get; set; }
+        public List<VoiceActor>? VoiceActors { get; set; }
     }
 
     public class Character
@@ -27,40 +27,40 @@ namespace Models
         public int MalId { get; set; }
 
         [JsonPropertyName("url")]
-        public string Url { get; set; }
+        public string? Url { get; set; }
 
         [JsonPropertyName("images")]
-        public CharacterImages Images { get; set; }
+        public CharacterImages? Images { get; set; }
 
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 
     public class CharacterImages
     {
         [JsonPropertyName("jpg")]
-        public CharacterImageData Jpg { get; set; }
+        public CharacterImageData? Jpg { get; set; }
 
         [JsonPropertyName("webp")]
-        public CharacterImageData Webp { get; set; }
+        public CharacterImageData? Webp { get; set; }
     }
 
     public class CharacterImageData
     {
         [JsonPropertyName("image_url")]
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
 
         [JsonPropertyName("small_image_url")]
-        public string SmallImageUrl { get; set; }
+        public string? SmallImageUrl { get; set; }
     }
 
     public class VoiceActor
     {
         [JsonPropertyName("person")]
-        public Person Person { get; set; }
+        public Person? Person { get; set; }
 
         [JsonPropertyName("language")]
-        public string Language { get; set; }
+        public string? Language { get; set; }
     }
 
     public class Person
@@ -69,18 +69,18 @@ namespace Models
         public int MalId { get; set; }
 
         [JsonPropertyName("url")]
-        public string Url { get; set; }
+        public string? Url { get; set; }
 
         [JsonPropertyName("images")]
-        public PersonImages Images { get; set; }
+        public PersonImages? Images { get; set; }
 
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 
     public class PersonImages
     {
         [JsonPropertyName("jpg")]
-        public object Jpg { get; set; }
+        public object? Jpg { get; set; }
     }
 }
